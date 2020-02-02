bd622423a673d11d77633b5b6422dfbd992e5528
Michael Carter <53894470+Carter-Prime@users.noreply.github.com>
2020-02-02 17:55:44 +0200

    Implemented left and right swipe gesture for the home activity.

---
+++ app/src/main/java/com/example/spicesinventory/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.spicesinventory.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spice_sqlite_test.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
