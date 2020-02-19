7f22772f7ce79514701b85afacd436f257f65683
astrid-st <59977231+astrid-st@users.noreply.github.com>
2020-02-19 07:21:49 +0200

    created User database and SignUpActivity

---
+++ app/src/main/java/com/example/spicesinventory/database/UserDao.java
@@ -0,0 +1,26 @@
+package com.example.spicesinventory.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insertUser(User user);
+
+    @Delete
+    void deleteUser(User user);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void upDate(User user);
+
+    @Query("SELECT * from users")
+    List<User> getAllUsers();
+
+}
