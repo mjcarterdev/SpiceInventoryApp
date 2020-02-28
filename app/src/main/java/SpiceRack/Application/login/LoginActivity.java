78c2bd1c2696920aca0a75d55d7bf84250ab7650
Michael Carter <53894470+Carter-Prime@users.noreply.github.com>
2020-02-28 13:16:38 +0200

    Merged with astrids current changes

--- app/src/main/java/SpiceRack/Application/login/LoginActivity.java
+++ app/src/main/java/SpiceRack/Application/login/LoginActivity.java
@@ -1,4 +1,91 @@
 package SpiceRack.Application.login;
 
-public class LoginActivity {
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.util.List;
+
+import SpiceRack.Application.activites.HomeActivity;
+import SpiceRack.Application.database.SpiceDatabase;
+import SpiceRack.Application.database.User;
+import SpiceRack.Application.database.UserDao;
+import SpiceRack.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText editEmailAddress, editPassword;
+    String emailID, emailAddress, editPasswordString, pw;
+    SpiceDatabase mySpiceRackDb;
+    UserDao myUserDao;
+    Button btnLogIn;
+    User tempEmail, tempPw;
+
+    private View.OnClickListener myClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.btnLogIn) {
+                logIn();
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        btnLogIn = findViewById(R.id.btnLogIn);
+        btnLogIn.setOnClickListener(myClick);
+
+        editEmailAddress = findViewById(R.id.editEmailAddress);
+        editPassword = findViewById(R.id.editPassword);
+
+        mySpiceRackDb = SpiceDatabase.getINSTANCE(this);
+        myUserDao = mySpiceRackDb.getUserDao();
+
+    }
+
+    public void logIn() {
+        emailAddress = editEmailAddress.getText().toString();
+        editPasswordString = editPassword.getText().toString();
+
+        tempEmail = myUserDao.getUserByEmail(emailAddress);
+        tempPw = myUserDao.getUserByPw(editPasswordString);
+
+        List<User> users = myUserDao.getAllUsers();
+
+        if(tempEmail != null)
+            emailID = tempEmail.getEmailAddress();
+
+        if(tempPw != null)
+            pw = tempPw.getPassword();
+
+        if (emailAddress.equals(emailID) && editPasswordString.equals(pw)){
+
+            myUserDao.getAllUsers();
+
+            //store logged in user to SharedPreferences
+            SharedPreferences prefPut = getSharedPreferences("User", Activity.MODE_PRIVATE);
+            SharedPreferences.Editor prefEditor = prefPut.edit();
+            prefEditor.putString("User logged in", emailAddress);
+            prefEditor.commit();
+
+            Intent openActivity = new Intent(this, HomeActivity.class);
+            startActivity(openActivity);
+
+        } else {
+            Toast.makeText(LoginActivity.this, "Wrong user or password", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
