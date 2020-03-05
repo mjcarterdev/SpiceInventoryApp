b967a1d7d1012501a79230b71bff7f75ee677699
Michael Carter <53894470+Carter-Prime@users.noreply.github.com>
2020-03-05 20:27:24 +0200

    started on the JavaDocs

--- app/src/main/java/SpiceRack/Application/activites/InventoryActivity.java
+++ app/src/main/java/SpiceRack/Application/activites/InventoryActivity.java
@@ -21,6 +21,16 @@ import SpiceRack.Application.database.SpiceListAdapter;
 import SpiceRack.Application.utilities.Navigation;
 import SpiceRack.databinding.InventoryActivityBinding;
 
+/**
+ *  <p>
+ *      InventoryActivity class creates and displays the Spice Inventory. It implements the custom
+ *      SpiceListAdapter with a onClickListener so when the item is pressed it passes the Spice object
+ *      to ScanActivity. The class has also has gesture control navigation.</p>
+ *
+ *  @author Michael and Astrid
+ *  @version 1.0
+ *  @since 05.03.2020
+ */
 public class InventoryActivity extends AppCompatActivity implements SpiceListAdapter.SpiceOnClickListener{
 
     InventoryActivityBinding inventoryLayout;
@@ -31,6 +41,11 @@ public class InventoryActivity extends AppCompatActivity implements SpiceListAda
     Navigation nav;
     private GestureDetectorCompat myGesture;
 
+    /**
+     *  <p>onCreate() calls and instantiates a singleton of the spice inventory database using Room.</p>
+     *
+     * @param savedInstanceState saved instance state of the activity.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,12 +62,18 @@ public class InventoryActivity extends AppCompatActivity implements SpiceListAda
         updateUI();
     }
 
+    /**
+     * On resuming the activity updateUI() is called to refresh the recycler view data.
+     */
     @Override
     protected void onResume() {
         super.onResume();
         updateUI();
     }
 
+    /* Check with Karri - the suggested notifyODataChange() method does not work. Currently using
+     * method below. Functional but not very efficient.
+     */
     public void updateUI() {
         spiceList = mySpiceDao.getAllSpices();
         Collections.sort(spiceList);
@@ -60,12 +81,31 @@ public class InventoryActivity extends AppCompatActivity implements SpiceListAda
         inventoryLayout.rvSpiceList.setAdapter(adapter);
     }
 
+    /**
+     * <p>
+     *     Analyzes the given motion event and if applicable triggers the appropriate callbacks on
+     *     the GestureDetector.OnGestureListener supplied.
+     * </p>
+     * @param event the current motion event
+     * @return true if the GestureDetector.OnGestureListener consumed the event, else false.
+     */
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         this.myGesture.onTouchEvent(event);
         return super.onTouchEvent(event);
     }
 
+    /**
+     * <p>
+     *     When an item from the recycler is pressed, spiceOnClick() is called. This creates an
+     *     intent that stores an extra String of the barcode information for the object at the
+     *     called position. The intent is to start the scanActivity class and pass the barcode with
+     *     it.
+     * </p>
+     * @param position is the location within the list of the recyclerview when an click event is
+     *                 registered.
+     */
     @Override
     public void spiceOnClick(int position) {
         String scannedBarcode = spiceList.get(position).getBarcode();
@@ -74,8 +114,26 @@ public class InventoryActivity extends AppCompatActivity implements SpiceListAda
         startActivity(intent);
     }
 
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+    /**
+     * <p>
+     *     MyGestureListener class extends GestureDetector.SimpleOnGestureListener a convenient class
+     *     where only a subset of gestures are required. This implements onDown() and onFLing() methods
+     *     but does nothing and return false.
+     * </p>
+     */
 
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        /**
+         * <p>
+         *      Swipe_Threshold is the distance travelled from the initial screen contact till the
+         *      final position in any of the four directions.
+         * </p>
+         * <p>
+         *      Swipe_Velocity_Threshold is the speed at which a movement needs to travel before being
+         *      identified as a gesture.
+         * </p>
+         * <p> The value given is an arbitrary number based on what feels right</p>
+         */
         private static final int SWIPE_THRESHOLD = 100;
         private static final int SWIPE_VELOCITY_THRESHOLD = 100;
 
@@ -83,6 +141,16 @@ public class InventoryActivity extends AppCompatActivity implements SpiceListAda
         public boolean onDown(MotionEvent e) {
             return false;
         }
+
+        /**
+         *
+         * @param downEvent The first down motion that started the fling.
+         * @param moveEvent The move motion event that triggered the current onFLing.
+         * @param velocityX The velocity of this fling measured in pixels per second along the x axis.
+         * @param velocityY The velocity of this fling measured in pixels per second along the y axis.
+         * @return Boolean. If false nothing will happen. If returned true the event has been completed
+         * successfully. The outcome of the return is dependent on the direction of this fling.
+         */
 
         @Override
         public boolean onFling(MotionEvent downEvent, MotionEvent moveEvent, float velocityX, float velocityY) {
--- app/src/main/java/SpiceRack/Application/activites/ScanActivity.java
+++ app/src/main/java/SpiceRack/Application/activites/ScanActivity.java
@@ -26,6 +26,16 @@ import SpiceRack.Application.database.SpiceListAdapter;
 import SpiceRack.Application.utilities.Navigation;
 import SpiceRack.databinding.ScanActivityBinding;
 
+/**
+ * <p>
+ *      Scan Activity class displays Spice Information based on information gathered
+ *      from barcode scanning or user clicks from the Spice Inventory activity. The activity shows
+ *      all the key information based on the selected spice. It displays, barcode number, spice name
+ *      and stock. While also displaying all the current spices with the same name that are in the
+ *      inventory database. The spice list can have its stock updated with swipe gesture.
+ * </p>
+ *
+ */
 
 public class ScanActivity extends AppCompatActivity implements SpiceListAdapter.SpiceOnClickListener {
 
@@ -38,6 +48,13 @@ public class ScanActivity extends AppCompatActivity implements SpiceListAdapter.
     Navigation nav;
     GestureDetectorCompat myGesture;
 
+    /**
+     * <p>
+     *      The barcode from passed intent is set to the activity. Database is
+     *      instantiated and sorted. Additional Spice information is gather from method
+     *      seachByBarcode() using barcode from intent.
+     * </p>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +82,14 @@ public class ScanActivity extends AppCompatActivity implements SpiceListAdapter.
         }
     }
 
+    /**
+     * <p>
+     *     searchByBarcode() takes the barcode string sent from the intent and retrieves the object
+     *     from the database. If the search parameter is null then default messages are set to the
+     *     activity. If not null then the objects data is retrieved and set to the activity for
+     *     display.
+     * </p>
+     */
     public void searchByBarcode(){
         mySpiceDao = mySpiceRackDb.getSpiceDao();
         receivedSpice = mySpiceDao.getSpiceByBarcode(scanLayout.tvDisplayBarcode.getText().toString());
@@ -85,6 +110,18 @@ public class ScanActivity extends AppCompatActivity implements SpiceListAdapter.
         }
     }
 
+    /**
+     * <p>
+     *      Check with Karri - the suggested notifyODataChange() method does not work. Currently
+     *      using method below. Functional but not very efficient.
+     * </p>
+     * <p>
+     *      Creates a list of Spice objects defined by Spice Name found from method
+     *      searchByBarcode(). List is sorted alphabetically and a total stock is calculated
+     *      using getStock() method on each list object. The sum is passed to receivedSpice.getInfo()
+     *      and is displayed within the activity.
+     * </p>
+     */
     public void updateUI() {
         spiceList = mySpiceDao.getSpiceByName(scanLayout.tvDisplaySpiceName.getText().toString());
         Collections.sort(spiceList);
@@ -101,15 +138,30 @@ public class ScanActivity extends AppCompatActivity implements SpiceListAdapter.
 
     @Override
     public void spiceOnClick(int position) {
-        Toast.makeText(this, "Spice is pressed", Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * <p>
+     *     A new ItemTouchHelper.SimpleCallback that @overrides onMove() and onSwipe() methods.
+     *     The onSwipe is used to add or subtract one from Spice object's stock variable.
+     *     The object is selected from the recyclerView by pressing and swiping either left or
+     *     right. A right swipe will subtract and a left swipe will add. Both actions will then
+     *     update the object within the Room database table.
+     * </p>
+     *
+     */
     ItemTouchHelper.SimpleCallback itemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
         @Override
         public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
             return false;
         }
 
+        /**
+         *
+         * @param viewHolder the attached RecyclerView from the layout that has the ItemTouchHelper
+         *                   assigned.
+         * @param direction predefined Android gesture recognition of swipe direction.
+         */
         @Override
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
             Spice spice = spiceList.get(viewHolder.getAdapterPosition());
@@ -127,13 +179,44 @@ public class ScanActivity extends AppCompatActivity implements SpiceListAdapter.
         }
     };
 
+    /**
+     * <p>
+     *     When an item from the recycler is pressed, spiceOnClick() is called. This creates an
+     *     intent that stores an extra String of the barcode information for the object at the
+     *     called position. The intent is to start the scanActivity class and pass the barcode with
+     *     it.
+     * </p>
+     * @param event the current motion event
+     * @return true if the GestureDetector.OnGestureListener consumed the event, else false.
+     */
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         this.myGesture.onTouchEvent(event);
         return super.onTouchEvent(event);
     }
 
+    /**
+     * <p>
+     *     MyGestureListener class extends GestureDetector.SimpleOnGestureListener a convenient class
+     *     where only a subset of gestures are required. This implements onDown() and onFLing() methods
+     *     but does nothing and return false.
+     * </p>
+     */
+
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        /**
+         * <p>
+         *      Swipe_Threshold is the distance travelled from the initial screen contact till the
+         *      final position in any of the four directions.
+         * </p>
+         * <p>
+         *      Swipe_Velocity_Threshold is the speed at which a movement needs to travel before being
+         *      identified as a gesture.
+         * </p>
+         * <p> The value given is an arbitrary number based on what feels right</p>
+         */
 
         private static final int SWIPE_THRESHOLD = 100;
         private static final int SWIPE_VELOCITY_THRESHOLD = 100;
@@ -142,6 +225,15 @@ public class ScanActivity extends AppCompatActivity implements SpiceListAdapter.
         public boolean onDown(MotionEvent e) {
             return false;
         }
+
+        /**
+         * @param downEvent The first down motion that started the fling.
+         * @param moveEvent The move motion event that triggered the current onFLing.
+         * @param velocityX The velocity of this fling measured in pixels per second along the x axis.
+         * @param velocityY The velocity of this fling measured in pixels per second along the y axis.
+         * @return Boolean. If false nothing will happen. If returned true the event has been completed
+         * successfully. The outcome of the return is dependent on the direction of this fling.
+         */
 
         @Override
         public boolean onFling(MotionEvent downEvent, MotionEvent moveEvent, float velocityX, float velocityY) {
